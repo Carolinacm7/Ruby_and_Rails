24dcb1a7ee6342d3f2ce8613415ff24671c6ad9a
Carolinacm7 <61048227+Carolinacm7@users.noreply.github.com>
2022-09-14 17:07:52 -0500

    [add]model - view - controller

---
+++ MVC.rb
@@ -0,0 +1,23 @@
+=begin 
+MVC
+Base de Datos <-> Modelo <-> Controlador -> Vista -> cliente 
+el framework se encarga de conectarlos 
+modelo vista controlador 
+el modelo = esla parte la la apicacion que se encarga de manejar los datos
+y todas las estructuras de datos, la conexion a la base de datos
+todo lo que tiene que ver con la recopilacion de datos manejo de 
+datos que van dentro del modelo
+
+el modelo se encarga de extrar los datos de la base de datos y mandarlos al
+controlador y el controlador  se los manda la vista 
+
+el controlador = el controlador es la parte que une toda la aplicacion 
+se encarga de concetar de comunicarse con la vista que es lo que
+se muestra al usurio y de comunicarse con el modelo 
+
+cuando el usuario entra a la aplicacion lo que se ejecuta
+es el controlador y se encarga de ver si el usuario paso algun parametro
+por ejm la url 
+
+la vista = es la aplicacion
+=end
